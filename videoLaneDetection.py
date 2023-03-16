f769b6a1b5e28a5a1d5b8bba21f19b84a957c4a8
Ryan Baker <baker.ryan787@gmail.com>
2023-03-16 18:48:58 -0400

    MA

--- videoLaneDetection.py
+++ videoLaneDetection.py
@@ -35,17 +35,19 @@ class Line():
         
         
     def update(self, new_line):
-        if self.cnt == 0 :
-            
-            if len(new_line):
-            
+
+        if len(new_line):
+            if self.cnt == 0 :            
                 p = np.polyfit(new_line[:,0], new_line[:,1], self.n_degrees)
                 (x_min, _), (x_max, _) = line1.min(0), line1.max(0)
 
                 self.limits = MvArray(np.array([x_min, x_max]), self.limits_alpha)
                 x_min, x_max = self.limits.array
 
-                self.axis = np.linspace(x_min, x_max, 10)
+                x_range = x_max - x_min
+
+
+                self.axis = np.linspace(x_min + x_range * 0.2 , x_max - x_range * 0.2, 10)
                 npred_line = np.polyval(p, self.axis) #predicted line
                 pred_line = npred_line
 
@@ -53,8 +55,7 @@ class Line():
 
                 self.cnt += 1
         
-        else:
-            if len(new_line):
+            else:
                 p = np.polyfit(new_line[:,0], new_line[:,1], self.n_degrees)
                 (nx_min, _), (nx_max, _) = new_line.min(0), new_line.max(0)
 
@@ -66,6 +67,7 @@ class Line():
 
                 self.predicted_line.update(npred_line)
                 self.cnt += 1
+            
             
     def get_line(self):
         try:
@@ -86,10 +88,10 @@ list_lane2 = []
 list_lane3 = []
 
 # create an instance for each line
-myline0 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 3)
-myline1 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 3)
-myline2 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 3)
-myline3 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 3)
+myline0 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 2)
+myline1 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 2)
+myline2 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 2)
+myline3 = Line(lines_alpha = 0.05, limits_alpha = 0.05, n_degrees = 2)
 
 directory="Test_Videos/To_Run"
 for file in os.listdir(directory):
@@ -116,46 +118,33 @@ for file in os.listdir(directory):
             continue
         if ret:
             lane_points, lane_detected, cfg=lane_detector.detect_lanes(frame) # Detect Lanes
-            pline=[None]*4
+            line0, line1, line2, line3 = lane_points
 
-            print(idx)
-            if len(list_lane1)<BUFFERSIZE:
-                print("if")
-                list_lane0.append(lane_points[0])
-                list_lane1.append(lane_points[1])
-                list_lane2.append(lane_points[2])
-                list_lane3.append(lane_points[3])
-                pline=lane_points
-                    
-            else:
-                print("Else")
-                list_lane0[idx]=lane_points[0]
-                list_lane1[idx]=lane_points[1]
-                list_lane2[idx]=lane_points[2]
-                list_lane3[idx]=lane_points[3]
-                # pline=lane_points
+            myline0.update(line0)
+            myline1.update(line1)
+            myline2.update(line2)
+            myline3.update(line3)
+            
+            line0 = myline0.get_line()
+            line1 = myline1.get_line()
+            line2 = myline2.get_line()[::-1]
+            line3 = myline3.get_line()[::-1]
 
-                # Do point manipulation
-                for i,(line0,line1,line2, line3) in enumerate(zip(list_lane0, list_lane1, list_lane2, list_lane3)): 
-    
-                    myline0.update(line0) # for each frame, update the line point values
-                    myline1.update(line1) # for each frame, update the line point values
-                    myline2.update(line2) # for each frame, update the line point values
-                    myline3.update(line3) # for each frame, update the line point values
-                    
-                    pline[0] = myline0.get_line() #this code gets the line points from the Line object
-                    pline[1] = myline1.get_line()
-                    pline[2] = myline2.get_line()
-                    pline[3] = myline3.get_line()
+            lane_points_ma = np.array([line0, line1, line2, line3])
 
-                # Increment idx or reset to beginning
-                idx=(idx+1)%BUFFERSIZE
-                
-            output_img=lane_detector.draw_lanes(frame, pline, lane_detected, cfg, True)
+            # print(lane_points_ma)
+            # print(lane_points)
+
+            output_img =lane_detector.draw_lanes(frame, lane_points, lane_detected, cfg, True)
+            output_img_ma =lane_detector.draw_lanes(frame, lane_points_ma.astype(int), np.array([1,1,1,1]), cfg, True)
 
             output_img=cv2.resize(output_img, size)
-            cv2.imshow("Detected lanes", output_img)
-            out.write(output_img)
+            output_img_ma=cv2.resize(output_img_ma, size)
+
+            out_stacked = np.vstack((output_img, output_img_ma))
+
+            cv2.imshow("Detected lanes", out_stacked)
+            out.write(out_stacked)
         else:
             break   
         if cv2.waitKey(1) == 27 or cv2.getWindowProperty('Detected lanes', 0)==-1:
